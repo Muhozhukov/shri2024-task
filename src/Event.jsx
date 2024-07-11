8c8b37db756e132437b4b3712f3803a5b7e68b74
Никита <pravoepredserdie@gmail.com>
2024-07-11 21:18:10 +0300

    add vite

---
+++ src/Event.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import { memo, useEffect, useRef } from "react";
+
+const Event = memo(function Event({
+    onSize,
+    slim,
+    icon,
+    iconLabel,
+    title,
+    subtitle
+}) {
+    const ref = useRef();
+
+    useEffect(() => {
+        const width = ref.current.offsetWidth;
+        const height = ref.current.offsetHeight;
+        if (onSize) {
+            onSize({ width, height });
+        }
+    }, [onSize]);
+
+    return <li ref={ref} className={'event' + (slim ? ' event_slim' : '')}>
+        <button className="event__button">
+            <span className={`event__icon event__icon_${icon}`} role="img" aria-label={iconLabel}></span>
+            <h4 className="event__title">{title}</h4>
+            {subtitle &&
+                <span className="event__subtitle">{subtitle}</span>
+            }
+        </button>
+    </li>;
+})
+
+export default Event;
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import './reset.css'
+import './styles.css'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <div>
+        <App /> 
+    </div>
+)
