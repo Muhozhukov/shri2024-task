210efd4ad85d6394f1c98399c03b28722101143c
Никита <pravoepredserdie@gmail.com>
2024-07-11 22:05:09 +0300

    refactor

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import React from "react";
 
 export default function Header() {
-    let [expanded, setExpanded] = useState(false);
-    let [toggled, setToggled] = useState(false);
+    let [expanded, setExpanded] = React.useState(false);
+    let [toggled, setToggled] = React.useState(false);
 
     const onClick = () => {
         if (!toggled) {
